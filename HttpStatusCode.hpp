cd42804072e4f3e78b71465793f1bd4222ec11b3
dnakano <dnakano@student.42tokyo.jp>
2021-03-18 10:39:47 +0900

    Mod: HttpStatusCode
    - add status code (in this page https://developer.mozilla.org/en-US/docs/Web/HTTP/Status )
    - add HTTP_NOMATCH for util functions

--- HttpStatusCode.hpp
+++ HttpStatusCode.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpStatusCode.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhasegaw <dhasegaw@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 20:33:54 by dhasegaw          #+#    #+#             */
-/*   Updated: 2021/03/05 09:21:44 by dhasegaw         ###   ########.fr       */
+/*   Updated: 2021/03/18 09:10:22 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 #define HTTPSTATUSCODE_HPP
 
 enum HTTPStatusCode {
+  HTTP_NOMATCH = -1, /* No Match to HTTP codes (used in internal function) */
+
   HTTP_100 = 100, /* Continue */
   HTTP_101 = 101, /* Switching Protoclols */
+  HTTP_102 = 102, /* Processing */
+  HTTP_103 = 103, /* Early Hints */
 
   HTTP_200 = 200, /* OK */
   HTTP_201 = 201, /* Created */
@@ -23,33 +27,61 @@ enum HTTPStatusCode {
   HTTP_203 = 203, /* Non-Authoritative Information */
   HTTP_204 = 204, /* No Content */
   HTTP_205 = 205, /* Reset Content */
+  HTTP_206 = 206, /* Partial Content */
+  HTTP_207 = 207, /* Multi-Status */
+  HTTP_208 = 208, /* Already Reported */
+  HTTP_226 = 226, /* IM Used */
 
   HTTP_300 = 300, /* Multiple choice */
   HTTP_301 = 301, /* Moved Parmanently */
   HTTP_302 = 302, /* Found */
   HTTP_303 = 303, /* See Other */
+  HTTP_304 = 304, /* Not Modified */
+  HTTP_305 = 305, /* Use Proxy */
+  HTTP_306 = 306, /* Unused */
   HTTP_307 = 307, /* Temporary Redirect */
+  HTTP_308 = 308, /* Permanent Redirect */
 
   HTTP_400 = 400, /* Bad Request */
+  HTTP_401 = 401, /* Payment Requied */
+  HTTP_403 = 403, /* Forbidden */
   HTTP_404 = 404, /* Forbidden */
   HTTP_405 = 405, /* Method Not Allowed */
   HTTP_406 = 406, /* Not Acceptable */
+  HTTP_407 = 407, /* Proxy Authentication Required */
   HTTP_408 = 408, /* Request Timeout */
   HTTP_409 = 409, /* Conflict */
-  HTTP_409 = 410, /* Gone */
+  HTTP_410 = 410, /* Gone */
   HTTP_411 = 411, /* Length Required */
-  HTTP_413 = 413, /* Pyaload Too Large */
-  HTTP_415 = 415, /* URI Too Long */
+  HTTP_412 = 412, /* Precondition Failed */
+  HTTP_413 = 413, /* Payload Too Large */
+  HTTP_414 = 414, /* URI Too Long */
+  HTTP_415 = 415, /* Unsupported Media Type */
+  HTTP_416 = 416, /* Range Not Satisfiable */
   HTTP_417 = 417, /* Exception Failed */
+  HTTP_418 = 418, /* I'm a teapot */
+  HTTP_421 = 421, /* Misdirected Request */
+  HTTP_422 = 422, /* Unprocessable Entity */
+  HTTP_423 = 423, /* Locked */
+  HTTP_424 = 424, /* Failed Dependency */
+  HTTP_425 = 425, /* Too Early */
   HTTP_426 = 426, /* Upgrade Required */
-  HTTP_426 = 426, /* Upgrade Required */
+  HTTP_428 = 428, /* Precondition Required */
+  HTTP_429 = 429, /* Too Many Requests */
+  HTTP_431 = 431, /* Request Header Fields Too Large */
+  HTTP_451 = 451, /* Unabailable For Legal Reasons */
 
   HTTP_500 = 500, /* Internal Server Error */
   HTTP_501 = 501, /* Not Implemented */
   HTTP_502 = 502, /* Bad Gateway */
   HTTP_503 = 503, /* Service Unavailable */
   HTTP_504 = 504, /* Gateway Timeout */
-  HTTP_505 = 505  /* HTTP Version Not Supported */
+  HTTP_505 = 505, /* HTTP Version Not Supported */
+  HTTP_506 = 506, /* Variant Also Negotiates */
+  HTTP_507 = 507, /* Insufficient Storage */
+  HTTP_508 = 508, /* HTTP Version Not Supported */
+  HTTP_510 = 510, /* Not Extended */
+  HTTP_511 = 511  /* Network Authentication Required */
 };
 
 #endif /* HTTPSTATUSCODE_HPP */
