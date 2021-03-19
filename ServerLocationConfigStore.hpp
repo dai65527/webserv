3b191ded6677949afae57753846a332a257dc8fa
dnakano <dnakano@student.42tokyo.jp>
2021-03-19 11:48:09 +0900

    Add: function of ServerLocationConfigStore
    - coplien
    - getters
    - parseUploadPass

--- ServerLocationConfigStore.hpp
+++ ServerLocationConfigStore.hpp
@@ -6,13 +6,14 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 18:59:06 by dnakano           #+#    #+#             */
-/*   Updated: 2021/03/12 19:50:24 by dnakano          ###   ########.fr       */
+/*   Updated: 2021/03/19 11:42:38 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVERLOCATIONCONFIGSTORE_HPP
 #define SERVERLOCATIONCONFIGSTORE_HPP
 
+#include <list>
 #include <string>
 
 /*
@@ -26,6 +27,7 @@ class ServerLocationConfigStore {
  protected:
   std::string upload_pass_;
   std::string upload_store_;
+
  public:
   // coplien
   ServerLocationConfigStore();
@@ -38,18 +40,18 @@ class ServerLocationConfigStore {
   const std::string& getUploadStore() const;
 
   /*
-  ** persers
+  ** parsers
   **
   ** Take content settings as string and then parse it and store the results.
-  ** Return values:
-  **  0: successfully parsed and stored the result
-  **  1: syntax error
-  **  2: wrong value was set
-  **  3: duplicate value (only for directive cannot be duplicated)
+  **
+  ** These function may throw on error.
+  **  - syntax error
+  **  - wrong value was set
+  **  - duplicate value (only for directive cannot be duplicated)
   */
 
-  int parseUploadPass(const std::string& settings);
-  int parseUploadStore(const std::string& settings);
+  void parseUploadPass(const std::list<std::string>& settings);
+  void parseUploadStore(const std::list<std::string>& settings);
 };
 
 #endif /* SERVERLOCATIONCONFIGSTORE_HPP */
