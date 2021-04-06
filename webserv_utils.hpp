35da58d9896019f7814fe38c31f0dce379ea0e2a
dnakano <dnakano@student.42tokyo.jp>
2021-04-06 16:23:00 +0900

    add getTimeStamp
    to webdserv utils

--- webserv_utils.hpp
+++ webserv_utils.hpp
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 09:06:32 by dnakano           #+#    #+#             */
-/*   Updated: 2021/03/28 16:03:45 by dnakano          ###   ########.fr       */
+/*   Updated: 2021/04/06 16:18:17 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 
 #include "HttpStatusCode.hpp"
 
+#include <string>
+
 HTTPStatusCode isHttpStatusCode(int code);
 
 bool isDirectory(const std::string& path);
+size_t getTimeStamp(char* buf, size_t bufsize, const char* fmt);
 
 #endif /* WEBSERV_UTILS_HPP */
